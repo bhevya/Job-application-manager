1b55e3254aba5dc85d42ec08e196e8c08e5a20c4
Bhavya Vineet <bhavya07vineet@gmail.com>
2019-04-09 04:09:03 -0400

    Version 2

--- test.py
+++ test.py
@@ -4,7 +4,8 @@ import argparse
 from datetime import date
 import pickle
 import os
-data={}
+from collections import defaultdict
+data=defaultdict(list)
 refwait={}
 if os.path.getsize('data.p')>0:
     with open('data.p', 'rb') as fp:
@@ -21,28 +22,40 @@ parser.add_argument("-ck","--checkcom",nargs='+',help="To Check whether I have a
 args = parser.parse_args()
 
 if args.insert:
-    print(args.insert)
-    for i in args.insert:
-        data[i]="waiting"
-        refwait[i]=1
+    for i in range(len(args.insert)):
+        if i==1:
+            data[args.insert[0]].append(args.insert[i])
+        elif i==2:
+            data[args.insert[0]].append(args.insert[i])
+
+    data[args.insert[0]].append("waiting")
+    refwait[args.insert[0]]=1
 elif args.applied:
-    for i in args.applied:
-        data[i]="applied"
-        try:
-            del refwait[i]
-        except:
-            print("Key missing",i)
+    for i in range(len(args.applied)):
+        if i==0 and data.get(args.applied[0],-1)!=-1:
+            data[args.applied[i]]=[]
+        elif i==1:
+            data[args.applied[0]].append(args.applied[i])
+    try:
+        data[args.applied[0]][2]="applied"
+    except:
+        data[args.applied[0]].append("applied")
+    try:
+        del refwait[i]
+        print("Done")
+    except:
+        print("Done")
 
 elif args.check:
     ans=[]
     for i,j in data.items():
-        if j=="waiting":
+        if j[2]=="waiting":
             ans.append(i)
     print(ans)
 elif args.total:
     ans=0
     for i,j in data.items():
-        if j=="applied":
+        if j[2]=="applied":
             ans+=1
     print("Total companies applied to:",ans)
 elif args.checkcom:
@@ -63,13 +76,15 @@ if (today>yesterday):
                 ans.append(j)
 fo = open("/home/bhavya/Desktop/companies.txt", "w")
 for company,status in data.items():
-    if status=="waiting" and refwait[company]>3:
+    if status[2]=="waiting" and refwait[company]>3:
         fo.write("Pls apply ASAP"+str(company)+"\n")
-    elif status=="waiting" and refwait[company]>2:
+    elif status[2]=="waiting" and refwait[company]>2:
         fo.write(str(company)+"\n")
 
-print(data)
-with open('data.p', 'wb') as fp:
-    pickle.dump(data, fp, protocol=pickle.HIGHEST_PROTOCOL)
-with open('refwait.p', 'wb') as fp:
-    pickle.dump(refwait, fp, protocol=pickle.HIGHEST_PROTOCOL)
+
+
+if len(data)>0:
+    with open('data.p', 'wb') as fp:
+        pickle.dump(data, fp, protocol=pickle.HIGHEST_PROTOCOL)
+    with open('refwait.p', 'wb') as fp:
+        pickle.dump(refwait, fp, protocol=pickle.HIGHEST_PROTOCOL)
